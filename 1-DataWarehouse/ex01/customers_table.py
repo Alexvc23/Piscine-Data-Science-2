e5796845211cf206843fdadb3d8f39400c2158d2
Jairo alexander Valencia candamil <jvalenci@z3r6p1.42lyon.fr>
2025-03-13 09:25:55 +0100

    Remove unused images and add `pie.py` for event visualization
    - Deleted `image.png` and `image-1.png` from `1-DataWarehouse`.
    - Added `2-DataAnalyst/ex00/pie.py` to visualize event distribution.
    - Uses SQL query to fetch event counts from `customers` table.
    - Generates a pie chart with color-coded event types.

---
+++ 1-DataWarehouse/ex01/customers_table.py
@@ -0,0 +1,28 @@
+import psycopg2
+
+dbname = "piscineds"
+user = "jvalenci"
+password = "mysecretpassword"
+host = "localhost"
+port = "5432"
+
+try:
+    with open("ex01/customers_table.sql", "r") as sql_file:
+        sql_script = sql_file.read()
+    print("SQL code has been imported!")
+    conn = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
+    print("Connected to postgres!")
+    cursor = conn.cursor()
+    cursor.execute(sql_script)
+    print("SQL script executed successfully!")
+    conn.commit()
+    cursor.close()
+    conn.close()
+except Exception as e:
+    print(f"Error: {str(e)}")
